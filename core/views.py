6ce4d7f29199aba4582af6a2f9e308a10c688dfe
Danny Mtwenty <dannywanyoike@gmail.com>
2022-12-23 21:07:42 +0300

    user suggestions

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from itertools import chain
+import random
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
@@ -32,9 +33,35 @@ def index(request):
    
 
     feed_list = list(chain(*feed))   
+
+    # user suggestion starts
+    all_users = User.objects.all()
+    user_following_all = []
+
+    for user in user_following:
+        user_list = User.objects.get(username=user.user)
+        user_following_all.append(user_list)
+    
+    new_suggestions_list = [x for x in list(all_users) if (x not in list(user_following_all))] #skip followesd user
+    current_user = User.objects.filter(username=request.user.username) #logged in user
+    final_suggestions_list = [x for x in list(new_suggestions_list) if ( x not in list(current_user))]
+    random.shuffle(final_suggestions_list)
+
+    username_profile = []
+    username_profile_list = []
+
+    for users in final_suggestions_list:
+        username_profile.append(users.id)
+
+    for ids in username_profile:
+        profile_lists = Profile.objects.filter(id_user=ids)
+        username_profile_list.append(profile_lists)
+
+    suggestions_username_profile_list = list(chain(*username_profile_list))
  
     context['user_profile'] = user_profile
     context['posts'] = feed_list
+    context['suggestions_username_profile_list'] = suggestions_username_profile_list
 
     return render(request, 'index.html',context)
 
